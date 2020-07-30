04d0eeedb0f2547eddb803db5d884db12f6e67ee
Serhii777 <muzykasv72@gmail.com>
2020-07-31 01:02:47 +0300

    Added all scss

--- src/js/countries-search.js
+++ src/js/countries-search.js
@@ -1,90 +1,53 @@
 // import coountriesServices from './services/countries-services';
+import newService from './services/fetchCountries';
+import spinner from './spinner';
+import { noticeInfo, noticeError, mySuccess } from './services/notices';
+
 import countriesTemplate from '../templates/countries-template.hbs';
 import listCountriesTemplate from '../templates/countries-list-template.hbs';
-import spinner from './spinner';
 
-import '../sass/styles.scss';
-import { error, alert, Stack } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
-import * as Confirm from '@pnotify/confirm';
-import '@pnotify/confirm/dist/PNotifyConfirm.css';
-
-const baseUrl = 'https://restcountries.eu/rest/v2/name/';
 const _ = require('lodash');
 
 const refs = {
-  input: document.querySelector('#search-input'),
+  searchForm: document.querySelector('#search-form'),
   countriesList: document.querySelector('#countries-list'),
-  countriesItem: document.getElementsByClassName('.card'),
   clearButton: document.querySelector('#clear-button'),
 };
 
 const inputDelay = _.debounce(searchInputHandler, 500);
 
-refs.input.addEventListener('input', inputDelay);
+refs.searchForm.addEventListener('input', inputDelay);
 refs.clearButton.addEventListener('click', resetPage);
 
 function searchInputHandler(event) {
   spinner.show();
-  let inputValue = event.target.value;
 
-  fetch(baseUrl + inputValue)
-    .then(response => {
-      return response.json();
-    })
+  let searchQuery = event.target.value;
+
+  newService
+    .fetchCountries(searchQuery)
     .then(data => {
-
       if (data.length >= 10) {
         spinner.hide();
-        console.log('Сделай запрос более специфичным');
-        notice();
+
+        if (searchQuery.length <= 1) {
+          return noticeInfo();
+        }
       } else if (data.length > 2 && data.length < 10) {
         spinner.hide();
-        console.log('Покажи список стран');
-        refs.countriesList.innerHTML = '';
+        resetPage();
         renderListCountries(data);
       } else {
         spinner.hide();
-        console.log('Сделай карточку страны');
-        refs.countriesList.innerHTML = '';
+        resetPage();
         renderCountriesRows(data);
+        mySuccess();
       }
     })
     .catch(error => {
-      console.log(error);
+      noticeError();
+      resetPage();
     });
-}
-
-function notice() {
-  error({
-    title: 'Attention!!!',
-    text: 'Too many matches found. Please enter a more specific query!',
-    width: '300px',
-    type: 'error',
-    delay: 1500,
-    remove: true,
-    modal: true,
-    overlayClose: true,
-    context: refs.input,
-    modules: new Map([
-      [
-        Confirm,
-        {
-          confirm: true,
-          buttons: [
-            {
-              text: 'Ok',
-              primary: true,
-              click: notice => {
-                notice.close();
-              },
-            },
-          ],
-        },
-      ],
-    ]),
-  });
 }
 
 function renderListCountries(countries) {
